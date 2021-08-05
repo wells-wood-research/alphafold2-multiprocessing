76e334313f6bbc918a5729ca5972f54f3163094b
universvm <leonardo.castorina98@gmail.com>
2021-08-05 18:21:16 +0100

    Fix dependencies

--- start.sh
+++ start.sh
@@ -1,13 +1,15 @@
 conda create --name fold python=3.7
+conda deactivate
 conda activate fold
-mkdir AF2
-cd AF2
 chmod u+x dependency_install.bsh
-./dependency_install.bsh
+chmod u+x msa2.bsh
+bash dependency_install.bsh
 conda install -c conda-forge jq
 conda install -c conda-forge curl
 conda install cudatoolkit cudnn cupti 
-cd ./alphafold_/
+cd alphafold_/
+pip install -r requirements.txt
 pip install .
 cd ../
 pip install ipywidgets
+pip install matplotlib
